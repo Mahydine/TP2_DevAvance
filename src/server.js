19b59b51fada6843d172a748fe6866442d5f740f
Mahydine <mlazzouli.pro@gmail.com>
2024-02-04 20:34:16 +0100

    fixed file path

--- src/server.js
+++ src/server.js
@@ -5,8 +5,8 @@ import fs from "node:fs"
 
 import { getData } from './api.js';
 
-handlebars.registerPartial('header', fs.readFileSync("templates/header.hbs", "utf-8"));
-handlebars.registerPartial('footer', fs.readFileSync("templates/footer.hbs", "utf-8"));
+handlebars.registerPartial('header', fs.readFileSync("src/templates/header.hbs", "utf-8"));
+handlebars.registerPartial('footer', fs.readFileSync("src/templates/footer.hbs", "utf-8"));
 
 const app = Fastify()
 
@@ -20,7 +20,7 @@ async function runServer(){
     const comicsData = await getData("https://gateway.marvel.com:443/v1/public/characters?apikey=a1300ede5d035aa6c05e527046c7f033");
 
     app.get('/', (req, res) => {
-        return res.view('templates/index.hbs', {comics: comicsData})
+        return res.view('src/templates/index.hbs', {comics: comicsData})
     })
 
     app.listen({ port: 3000 })
